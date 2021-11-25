cf89d56cee76537d5ea527016ccf334d3acf9693
cjuan-code <cjuaniestacio@gmail.com>
2021-11-25 16:32:50 +0100

    change deployment ports

--- backend/go/main.go
+++ backend/go/main.go
@@ -9,6 +9,6 @@ func main() {
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run(":3000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
