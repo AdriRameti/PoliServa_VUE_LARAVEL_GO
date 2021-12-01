25392e649faefcdaa9442dc8f624ceb9a59f6d26
AdriRameti <adri7agu@gmail.com>
2021-12-01 21:47:31 +0100

    Create Routing and Controllers

--- backend/laravel/routes/api.php
+++ backend/laravel/routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use app\Http\Controllers\CourtsController;
+use app\Http\Controllers\ReservationController;
+use app\Http\Controllers\SportsController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::resouce('reservation',ReservationController::class);
+Route::resouce('courts',CourtsController::class);
+Route::resouce('sports',SportsController::class);
