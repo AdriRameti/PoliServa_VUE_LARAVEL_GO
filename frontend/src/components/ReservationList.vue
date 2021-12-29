6cbd497892eded9e07e4de6061cff19ad48d7fc3
AdriRameti <adri7agu@gmail.com>
2021-12-29 21:13:40 +0100

    Update Vue functions

---
+++ frontend/src/components/ReservationList.vue
@@ -0,0 +1,42 @@
+<template>
+            <div class="pistas d-flex flex-wrap p-3 justify-content-center m-3">
+            <div class="card p-3 card-reser">
+                <img src="http://manzasport.com/wp-content/uploads/2018/04/Pista-de-padel-Paquito-Navarro-by-Manzasport-1.jpeg"/>
+                <h1>Pista 1</h1>
+                <p class="reser-descrip">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+            </div>
+            <div class="card p-3 card-reser">
+                <img src="http://manzasport.com/wp-content/uploads/2018/04/Pista-de-padel-Paquito-Navarro-by-Manzasport-1.jpeg"/>
+                <h1>Pista 1</h1>
+                <p class="reser-descrip">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+            </div>
+            <div class="card p-3 card-reser">
+                <img src="http://manzasport.com/wp-content/uploads/2018/04/Pista-de-padel-Paquito-Navarro-by-Manzasport-1.jpeg"/>
+                <h1>Pista 1</h1>
+                <p class="reser-descrip">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+            </div>
+            <div class="card p-3 card-reser">
+                <img src="http://manzasport.com/wp-content/uploads/2018/04/Pista-de-padel-Paquito-Navarro-by-Manzasport-1.jpeg"/>
+                <h1>Pista 1</h1>
+                <p class="reser-descrip">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+            </div>
+            <div class="card p-3 card-reser">
+                <img src="http://manzasport.com/wp-content/uploads/2018/04/Pista-de-padel-Paquito-Navarro-by-Manzasport-1.jpeg"/>
+                <h1>Pista 1</h1>
+                <p class="reser-descrip">Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+            </div>
+            <div class="card p-3 card-reser">
+                <img src="http://manzasport.com/wp-content/uploads/2018/04/Pista-de-padel-Paquito-Navarro-by-Manzasport-1.jpeg"/>
+                <h1>Pista 1</h1>
+                <p class="reser-descrip">Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+            </div>
+        </div> 
+</template>
+<script>
+
+export default({
+    setup() {
+        
+    },
+})
+</script>
