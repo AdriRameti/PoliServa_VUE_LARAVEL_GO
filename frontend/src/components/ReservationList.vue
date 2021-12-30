ed9d80a39dc9722e2ad6e7dfcf530b732b5f0981
cjuan-code <cjuaniestacio@gmail.com>
2021-12-30 01:27:12 +0100

    get all courts composable added

--- frontend/src/components/ReservationList.vue
+++ frontend/src/components/ReservationList.vue
@@ -1,4 +1,11 @@
 <template>
+    <div>
+        <ul>
+            <li v-for="court in courts" :key="court.id">
+                {{ court.id_sport }}
+            </li>
+        </ul>
+    </div>
             <div class="pistas d-flex flex-wrap p-3 justify-content-center m-3">
             <div class="card p-3 card-reser">
                 <img src="http://manzasport.com/wp-content/uploads/2018/04/Pista-de-padel-Paquito-Navarro-by-Manzasport-1.jpeg"/>
@@ -34,9 +41,15 @@
 </template>
 <script>
 
+import { useGetAllCourts } from '../composables/courts/useGetAllCourts';
+
 export default({
     setup() {
-        
+        const { courts, count } = useGetAllCourts();
+
+        console.log(courts);
+
+        return { courts, count };
     },
 })
 </script>
---
+++ frontend/src/composables/courts/useGetAllCourts.js
@@ -0,0 +1,20 @@
+import { ref, watch, computed } from "vue";
+// import Api from "../../services/Api"
+import { secret } from "../../secret";
+
+export const useGetAllCourts = async () => {
+
+    let courts = ref([]);
+
+    let count = computed(() => courts.value.length);
+
+    let res = await fetch(secret.GO_URL + "courts/");
+    // let res = Api(`http://localhost:3000/api/`).get(`courts`);
+    let data = await res.json();
+    
+    courts.value = data;
+
+    console.log(courts.value, count.value);
+
+    return { courts, count }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+}
