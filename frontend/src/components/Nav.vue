d020d8ff7a4291e288062464b115d8876e6a083f
cjuan-code <cjuaniestacio@gmail.com>
2022-01-02 21:27:08 +0100

    change navbar

--- frontend/src/components/Nav.vue
+++ frontend/src/components/Nav.vue
@@ -13,8 +13,12 @@
           <li class="nav-item rounded">
             <a class="nav-link text-white" v-on:click="clearSport()"><i class="bi bi-people-fill me-2"></i>Reservar</a>
           </li>
+
+          <li class="nav-item rounded" v-show="!token">
+              <a class="nav-link active text-white" aria-current="page" href="/#/login"><i class="bi bi-house-fill me-2"></i>Login</a>
+          </li>
           
-          <li class="nav-item dropdown rounded">
+          <li class="nav-item dropdown rounded" v-show="token">
             <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="bi bi-person-fill me-2"></i>Profile</a>
             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
               <li><a class="dropdown-item" href="#">Account</a></li>
@@ -44,6 +48,12 @@
 <script>
 
 export default({
+
+  data() {
+    return {
+      token: localStorage.getItem('token') ? true : false
+    }
+  },
   methods:{
     clearSport(slug) {
       localStorage.removeItem('slug');
@@ -51,7 +61,7 @@ export default({
     }
   },
   setup() {
-        
+    
   },
 })
 </script>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -18,7 +18,7 @@ const routes = [
   {path: '/court',name: 'Court',component:()=> import('../views/Court/Court.vue')},
   {path: '/dashboard',name: 'Dashboard',component:()=> import('../views/Dashboard/Dashboard.vue')},
   {path: '/login',name: 'Login',component:()=> import('../views/Login/Login.vue')},
-  {path: '/register',name: 'Register',component:()=> import('../views/Login/Register.vue')},
+  // {path: '/register',name: 'Register',component:()=> import('../views/Login/Register.vue')},
   {path: '/reservation',name: 'Reservation',component:()=> import('../views/Reservation/Reservation.vue')},
   {path: '/sport',name: 'Sport',component:()=> import('../views/Sport/Sport.vue')}
 ]
