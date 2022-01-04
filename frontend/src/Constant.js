a8f3f4f7a3e56508e6b03d6992227ca7b9e6841a
cjuan-code <cjuaniestacio@gmail.com>
2022-01-04 21:18:32 +0100

    added user store with constants

---
+++ frontend/src/Constant.js
@@ -0,0 +1,6 @@
+export default {
+    LOGIN_USER:"loginUser",
+    REGISTER_USER:"registerUser",
+    VALIDATE_OTP:"validateOTP",
+    VALIDATE_REGISTER:"validateRegister"
+}
---
+++ frontend/src/store/modules/user.js
@@ -0,0 +1,40 @@
+import Constant from "../../Constant";
+
+export const user = {
+    namespaced: true,
+
+    state: {
+
+    },
+    mutations: {
+        [Constant.LOGIN_USER]: (state, payload) => {
+
+        },
+        [Constant.REGISTER_USER]: (state, payload) => {
+            
+        },
+        [Constant.VALIDATE_OTP]: (state, payload) => {
+            
+        },
+        [Constant.VALIDATE_REGISTER]: (state, payload) => {
+            
+        }
+    },
+    actions: {
+        [Constant.LOGIN_USER]: (state, payload) => {
+
+        },
+        [Constant.REGISTER_USER]: (state, payload) => {
+            
+        },
+        [Constant.VALIDATE_OTP]: (state, payload) => {
+            
+        },
+        [Constant.VALIDATE_REGISTER]: (state, payload) => {
+            
+        }
+    },
+    getters: {
+
+    }
+}
