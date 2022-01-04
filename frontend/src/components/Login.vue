e5f66253c973fdd408bd31a539ed691493ad8b53
cjuan-code <cjuaniestacio@gmail.com>
2022-01-04 18:16:09 +0100

    login page and toastr added

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,11 +1,104 @@
 <template>
-  <h1>Hola login</h1>
+  <section>
+    <div class="container py-5 h-100">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+          <div class="card shadow-2-strong" style="border-radius: 1rem;">
+            <div class="card-body p-4 text-center">
+
+              <h3 class="mb-3">Sign in</h3>
+
+              <div class="form-outline mb-4">
+                <input type="email" id="typeEmailX-2" v-model="email" class="form-control form-control-lg" placeholder="Email"/>
+              </div>
+
+              <div class="form-outline mb-4">
+                <input type="password" id="typePasswordX-2" v-model="password" class="form-control form-control-lg" placeholder="Password"/>
+              </div>
+
+              <!-- Checkbox -->
+              
+              <button class="btn btn-primary btn-lg btn-block" v-on:click="login()">Login</button>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 
+import { useToast } from "vue-toastification";
+
 export default({
 
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    validateForm() {
+      var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+      if (this.email.length == 0 && this.password.length == 0) {
+        return "empty";
+      } else if (this.email.length == 0) {
+        return "email empty";
+      } else if (this.password.length == 0) {
+        return "password empty";
+      }
+
+      if (regex.test(this.email) == false) { 
+        return "email not valid";
+      }
+
+    },
+    login() {
+
+      const toastr = useToast();
+      var val = this.validateForm();
+
+      if (val == "empty") {
+
+        toastr.error("Email and password mustn't be empty", {
+          timeout: 1500
+        });
+        return
+
+      } else if (val == "email empty") {
+
+        toastr.error("Email mustn't be empty", {
+          timeout: 1500
+        });
+        return
+
+      } else if (val == "password empty") {
+
+        toastr.error("Password mustn't be empty", {
+          timeout: 1500
+        });
+        return
+
+      } else if (val == "email not valid") {
+
+        toastr.error("Email must be like example@example.example", {
+          timeout: 1500
+        });
+        return
+
+      }
+
+      toastr.success("Login success", {
+        timeout: 1500
+      });
+
+
+    }
+  },
   setup() {
     
   },
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
+import Toast from "vue-toastification"
+import "vue-toastification/dist/index.css"
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'bootstrap'
 import './assets/app.scss'
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(Toast).mount('#app')
 
 
