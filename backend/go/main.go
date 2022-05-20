1e56d43a98089662284450d72e7c4e37c849abeb
AdriRameti <adri7agu@gmail.com>
2022-05-20 14:06:42 +0200

    Wait to connect to DB

--- backend/go/main.go
+++ backend/go/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-
+	"fmt"
 	"poliserva/Config"
 	"poliserva/Routes"
-	"fmt"
+	"time"
+
 	"github.com/jinzhu/gorm"
-	
 )
 
 var err error
@@ -14,11 +14,26 @@ var err error
 func main() {
 
 	Config.DB, err = gorm.Open("mysql", Config.DbURL(Config.BuildDBConfig()))
-
+	count := 0
 	if err != nil {
 		fmt.Println("Status:", err);
+		for{
+			if err == nil{
+				fmt.Println("Status is NIL")
+				break
+			}
+			fmt.Println("Status is not NIL")
+			time.Sleep(time.Second)
+			count++
+			if count > 180{
+				fmt.Printf("***************")
+				fmt.Println("DB Connection failure")
+				panic(err)
+			}
+			Config.DB, err = gorm.Open("mysql", Config.DbURL(Config.BuildDBConfig()))
+		}
 	}
-
+	fmt.Println("DB Connection successful")
 	defer Config.DB.Close()
 
 	r := Routes.SetupRouter()
