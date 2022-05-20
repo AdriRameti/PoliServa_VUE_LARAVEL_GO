77e225c88abccaeeab4d1a40a9d7ad91ebc69bbb
cjuan-code <cjuaniestacio@gmail.com>
2022-05-20 20:47:24 +0200

    change tests

--- frontend/cypress/integration/3-poliserva/LoginUsersMode.spec.js
+++ frontend/cypress/integration/3-poliserva/LoginUsersMode.spec.js
@@ -1,38 +1,41 @@
 const time = 3000
 context('Login with all modes and show the profile and user information',()=>{
 
-    //Can't show the Social mode because Cypress not support this
-    it('Social login and profile information',()=>{
+    it('Accept cookies', () => {
         cy.visit('http://localhost:4200/#/')
+        cy.get('.buttons__cta > button').click()
+    })
+
+    // Can't show the Social Login mode because Cypress does not support external windows
+    it('Social login and profile information',() => {
         cy.wait(time)
-        cy.get(':nth-child(6) > .nav-link').click()
-        cy.wait(time-1800)
+        cy.get(':nth-child(7) > .nav-link').click()
         cy.get(':nth-child(1) > .firebaseui-idp-button')
-        cy.wait(time)
         cy.get(':nth-child(1) > .nav-link')
-        cy.wait(time-2000)
         cy.get(':nth-child(6) > .nav-link').click()
         cy.wait(time-1800)
     })
 
-    it('Do the login',()=>{
+    it('User login',() => {
         cy.get('#typeEmailX-2').type('ejemplo@ejemplo.com')
         cy.wait(time-1800)
         cy.get('#typePasswordX-2').type('ejemplo')
-        cy.wait(time)
         cy.get(':nth-child(1) > section > .container > .row > .col-12 > .card > .card-body > .btn').click()
-        cy.wait(time)
     })
 
-    it('Show user information',()=>{
+    it('Show user information and access to dashboard',() => {
+        cy.wait(time-800)
         cy.get('#navbarDropdown').click()
-        cy.wait(time-1500)
         cy.get(':nth-child(1) > .dropdown-item').click()
-        //It has not been possible to access the profile and dashboard, the route returns to home for no reason
+        // It has not been possible to access the profile and dashboard, it seems Vue Guards don't let Cypress access to these routes    
+    })
+
+    it('Visit Contact and About Us', () => {
+        cy.wait(time-800)
         cy.get('.navbar-nav > :nth-child(3) > .nav-link').click()
         cy.wait(time+2000)
         cy.get('.navbar-nav > :nth-child(4) > .nav-link').click()
         cy.wait(time)
-        cy.get('footer').scrollIntoView({ duration: time })
+        cy.get('footer').scrollIntoView({ duration: time - 1000 })
     })
 })
